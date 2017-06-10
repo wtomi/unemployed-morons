b33b345951c1ce91b407b7e1591de18ff6f3acd4
tommy <tomi.git@gmail.com>
2017-06-10 20:22:37 +0200

    Init MPI

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <mpi/mpi.h>
 
-int main() {
+int main(int argc, char **argv) {
+
+    int rank;
+    int size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
