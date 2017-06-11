190fe4e8d7b23a8bc7d3583245c37355c0d4d8e8
tommy <tomi.git@gmail.com>
2017-06-11 23:31:53 +0200

    Add AgentInterface

---
+++ src/AgentInterface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tommy on 11.06.17.
+//
+
+#include "AgentInterface.h"
---
+++ src/AgentInterface.h
@@ -0,0 +1,15 @@
+//
+// Created by tommy on 11.06.17.
+//
+
+#ifndef UNEMPLOYED_MORRONS_AGENTINTERFACE_H
+#define UNEMPLOYED_MORRONS_AGENTINTERFACE_H
+
+
+class AgentInterface {
+public:
+    virtual void run() = 0;
+};
+
+
+#endif //UNEMPLOYED_MORRONS_AGENTINTERFACE_H
